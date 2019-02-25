3a73b46a51aaad9c1f44cc07be907187c08461f7
foliwe <foliwe@gmail.com>
2019-02-25 23:39:04 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
+#Rails.application.routes.url_helpers.url_for(controller: "admin/users", action: 'show', id: 1, host: 'somehost.org', port: '8080', subdomain: "yadah") do
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
-
   root to: 'posts#index'
 
   get 'home/index'
